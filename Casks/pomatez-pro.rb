2046c5126d0b9decd2f2862c42c7ecde78ee1ff3
Martin He <martinhe9527@gmail.com>
2026-09-24 05:21:04 +1000

    pomatez-pro: update to 2026.9.24

--- Casks/pomatez-pro.rb
+++ Casks/pomatez-pro.rb
@@ -1,6 +1,6 @@
 cask "pomatez-pro" do
   version "2026.9.24"
-  sha256 "6a03b8ccf52255e6b7d83b8d0f63e2b01e2a8b0ea7f04c6919dc37866dccba82"
+  sha256 "4400b418bd88bc4f95a570d08e8037e4de337bded33a7aa0a2ef4f4d5235d5ba"
 
   url "https://github.com/scyllarusllc/pomatez-releases/releases/download/v#{version}/Pomatez-Pro-#{version}.zip"
   name "Pomatez Pro"
